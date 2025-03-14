0c3743972f871cce93c36d0736dfd4f531d28a13
MyMQL <75244945+MyMQL@users.noreply.github.com>
2025-03-14 13:37:08 +0100

    1.6.0

---
+++ API/SCP372Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using MEC;
+
+namespace SCP372Plugin
+{
+    public class SCP372Manager
+    {
+
+        public IEnumerator<float> MonitorVisibilityState()
+        {
+            while (true)
+            {
+                yield return Timing.WaitForSeconds(1f);
+                VisibilityManager.Instance.MonitorState();
+            }
+        }
+    }
+}
